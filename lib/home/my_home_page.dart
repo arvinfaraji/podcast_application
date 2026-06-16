adec972b821a090b07a0f8bd75664358de75d451
arvinfaraji <arvinfaraji0086@gmail.com>
2026-06-16 23:43:56 +0330

    add slider section to home page

--- lib/home/my_home_page.dart
+++ lib/home/my_home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:kamionchi/utils/app_colors.dart' as AppColor;
 
@@ -9,6 +11,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  List? popularBooks;
+
+  readData() async {
+  try {
+    String s = await DefaultAssetBundle.of(context).loadString("json/popularBooks.json");
+    setState(() {
+      popularBooks = json.decode(s);
+    });
+  } catch (e) {
+    print("Error On Load File : $e");
+  }
+}
+
+
+  @override
+  void initState() {
+    super.initState();
+    readData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,7 +58,58 @@ class _MyHomePageState extends State<MyHomePage> {
                     )
                   ],
                 ),
-              )
+              ),
+
+              SizedBox(height: 20,),
+
+            //  title row
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20,),
+                    child: Text('Popular Books', style: TextStyle(fontSize: 20),),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 20,),
+
+              // slider section 
+              SizedBox(
+                height: 180,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      left: -20,
+                      right: 0,
+                      child: SizedBox(
+                        height: 180,
+                        child: PageView.builder(
+                          controller: PageController(viewportFraction: 0.8),
+                          itemCount: popularBooks == null ? 0 : popularBooks?.length,
+                          itemBuilder: (_, i) {
+                            return Container(
+                              height: 100,
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.only(right: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  // image: AssetImage('assets/images/mess.jpg'),
+                                  image: AssetImage(popularBooks?[i]["img"]),
+                                  fit: BoxFit.fill
+                                )
+                              ),
+                            );
+                          }
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              
             ],
           ),
         )
